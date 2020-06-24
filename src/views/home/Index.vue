6aa8bd026216ecf2d3c17813dc56e88e918949f8
uuoocckk-983 <799754081@qq.com>
2020-06-24 17:44:08 +0800

    6.24

---
+++ src/views/home/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="container">
+        <!-- 视频教程 -->
+        <VideoCourse></VideoCourse>
+        <!-- 技术分享 -->
+<!--        <ArticleShare></ArticleShare>-->
+    </div>
+</template>
+
+<script>
+    import VideoCourse from "../videoCourse/VideoCourse";
+    // import ArticleShare from "../articleShare/ArticleShare";
+    export default {
+        name: "Home",
+        components:{
+            VideoCourse,
+            // ArticleShare
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
